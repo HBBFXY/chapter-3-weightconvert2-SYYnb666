18d625be62d4cfacbba39912516874b89e07e1df
SYYnb666 <3638506532@qq.com>
2025-09-19 11:04:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# testweightcovert
-initial_weight = x
+initial_weight = float(input())
 moon_ratio = 0.165
 earth_weight = initial_weight
+print（"年份\t地球体重（公斤）\t月球体重（公斤）"）
 for year in range（1, 11）:
     moon_weight = earth_weight * moon_ratio
-    print（"第{}年\t{:.2f}\t\t{:.2f}".format（year,earth_weight, moon_weight））
+    print（"第{}年\t{:.2f}\t\t{:.2f}".format（year, earth_weight, moon_weight））
     earth_weight += 0.5
