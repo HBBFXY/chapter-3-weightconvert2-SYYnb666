db6a2b401df18479214568787e85332c6f503623
SYYnb666 <3638506532@qq.com>
2025-09-19 11:27:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-# 获取用户输入的初始体重
-initial_weight = float(())
-# 每年增长的体重
+initial_weight = float(input())
 annual_gain = 0.5
-# 月球体重是地球的比例
 moon_ratio = 0.165
 for year in range(1, 11):
-    # 计算地球上当年的体重
     earth_weight = initial_weight + annual_gain * year
-    # 计算月球上当年的体重
     moon_weight = earth_weight * moon_ratio
-    # 使用 format 控制输出格式，保留两位小数，使输出美观
     print("{}\t{:.2f}\t\t{:.2f}".format(year, earth_weight, moon_weigh))
