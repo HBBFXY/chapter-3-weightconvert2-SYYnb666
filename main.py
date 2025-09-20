4a7e58a0ab09167596fb5d6d78e12575b5ffee8c
SYYnb666 <3638506532@qq.com>
2025-09-20 09:54:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# 在这个文件里编写代码
+# 计算未来10年地球和月球的重量变化
 earth_weight = float(input("请输入地球重量（kg）:"))
 print("年份\t地球重量(kg)\t月球重量(kg)")
-for year in range(1,11)）:
+for year in range(1,11):
     earth_weight+=0.5
     moon_weight=earth_weight*0.165
     print(f"{year}\t{earth_weight:.2f}\t\t{moon_weight:.2f}")
